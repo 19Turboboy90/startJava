2de28143b3d0061694aeaabb7c7a19191c0dbc83
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-01-07 14:51:44 +0300

    Added class Variable

---
+++ lesson_1/Variable/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte b = 6;
+        short s = 16;
+        int i = 3600;
+        long ln = 140000L;
+        float fl = 3417.343f;
+        double db = 375782.274;
+        char ch = 'f';
+        boolean bool = true;
+
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + ln);
+        System.out.println("float: " + fl);
+        System.out.println("double: " + db);
+        System.out.println("char: " + ch);
+        System.out.println("boolean: " + bool);
+    }
+}
