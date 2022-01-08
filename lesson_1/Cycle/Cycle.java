8d1c62adc78e3df46e282a46f01a426e465d2919
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-01-08 11:27:18 +0300

    Added class Cycle

---
+++ lesson_1/Cycle/Cycle.java
@@ -0,0 +1,34 @@
+public class Cycle {
+    public static void main(String[] args) {
+      //Cycle for  
+        for(int i = 0; i <= 20; i++) {
+            if (i == 20) {
+                System.out.println(i + ".");
+            } else {
+                System.out.print(i + ", ");
+            }  
+        }
+
+      //Cycle while
+        int value = -6;
+        while (value <= 6) {
+            if (value == 6) {
+                System.out.println(value + ".");
+            } else {
+                System.out.print(value + ", ");
+            }
+            value +=2;
+        }
+
+        // do while
+        int number = 10;
+        int tmp = 0;
+        do {
+            if (number % 2 == 1) {
+                tmp += number;
+            }
+            number++;
+        } while(number < 20);
+        System.out.println("sum of numbers: " + tmp);
+    }
+}
