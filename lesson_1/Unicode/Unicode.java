c64b7d090fca4a4078067e218e89f862c693ade0
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-01-10 08:24:40 +0300

    added fixes to the Unicode class

--- lesson_1/Unicode/Unicode.java
+++ lesson_1/Unicode/Unicode.java
@@ -2,7 +2,7 @@ public class Unicode {
     public static void main(String[] args) {
         char firstPosition = 33;
         char lastPosition = 126;
-        for (char ch = firstPosition; ch < lastPosition; ch++) {
+        for (char ch = firstPosition; ch <= lastPosition; ch++) {
             System.out.print(ch + " ");
         }
     }
