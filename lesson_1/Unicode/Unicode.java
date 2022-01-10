bb8e5a6057bb9124438f49197910463a4070f0e0
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-01-10 08:21:04 +0300

    Added class Unicode

---
+++ lesson_1/Unicode/Unicode.java
@@ -0,0 +1,9 @@
+public class Unicode {
+    public static void main(String[] args) {
+        char firstPosition = 33;
+        char lastPosition = 126;
+        for (char ch = firstPosition; ch < lastPosition; ch++) {
+            System.out.print(ch + " ");
+        }
+    }
+}
