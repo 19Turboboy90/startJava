05004a7c7e6a8c237a462aeffb5de681b0d44a84
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-01-18 16:35:07 +0300

    Added Person class

---
+++ lesson_2/Person/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String name = "Ilia";
+    char gender = 'm';
+    double weight = 85.3;
+    double height = 1.74;
+    int age = 31;
+
+    public void go() {
+        System.out.println("Идти");
+    }
+
+    public void sit() {
+        System.out.println("Сидеть");
+    }
+
+    public void run() {
+        System.out.println("Бежать");
+    }
+
+    public void speak() {
+        System.out.println("Говорить");
+    }
+
+    public void learnJava() {
+        System.out.println("Учить Java");
+    }
+}
