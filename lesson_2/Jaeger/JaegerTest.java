64ed8d506db2614bdb6c6faa30e2b754e4e22aab
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-01-21 16:22:11 +0300

    Added classes Jaeger and JaegerTest

---
+++ lesson_2/Jaeger/JaegerTest.java
@@ -0,0 +1,38 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger romeoBlue = new Jaeger();
+        Jaeger louisBancroft = new Jaeger();
+
+        romeoBlue.setModelName("Romeo Blue");
+        romeoBlue.setMark("Mark-1");
+        romeoBlue.setOrigin("USA");
+        romeoBlue.setHeight(77.72f);
+        romeoBlue.setWeight(7775);
+        romeoBlue.setSpeed(2);
+        romeoBlue.setStrength(7);
+        romeoBlue.setArmor(6);
+
+        louisBancroft.setModelName("Louis Bancroft");
+        louisBancroft.setMark("Mark-1");
+        louisBancroft.setOrigin("Canada");
+        louisBancroft.setHeight(79);
+        louisBancroft.setWeight(2980);
+        louisBancroft.setSpeed(10);
+        louisBancroft.setStrength(10);
+        louisBancroft.setArmor(10);
+
+        System.out.println(louisBancroft.getModelName() + " Вышел на охоту");
+        louisBancroft.scanningTerrain("Производится сканирование месности");
+        System.out.println(louisBancroft.getModelName());
+        louisBancroft.shoot();
+        System.out.println(romeoBlue.getModelName());
+        romeoBlue.move();
+        System.out.println(romeoBlue.getModelName());
+        romeoBlue.shoot();
+        System.out.println(louisBancroft.getModelName());
+        louisBancroft.shield();
+        System.out.println(louisBancroft.getModelName());
+        romeoBlue.shoot();
+        System.out.println(romeoBlue.getModelName() + " Побежден!!!");
+    }
+}
