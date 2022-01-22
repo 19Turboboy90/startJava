32fd10517646ed3f2aeab2236722d111e1bee755
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-01-22 19:30:48 +0300

    fixed syntax in the CalculatorTest class

--- lesson_2/Calculator/CalculatorTest.java
+++ lesson_2/Calculator/CalculatorTest.java
@@ -4,19 +4,19 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Scanner console = new Scanner(System.in);
-        String check = "";
+        String check = "yes";
         while (!check.equals("no")) {
-            System.out.print("Введите первое число: ");
-            calculator.setNumber1(console.nextLong());
-            System.out.print("Введите знак математической операции: ");
-            calculator.setOperator(console.next().charAt(0));
-            System.out.print("Введите второе число: ");
-            calculator.setNumber2(console.nextLong());
-            calculator.checkingOperator();
-            do {
-                System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-                check = console.next();
-            } while (!check.equals("yes") && !check.equals("no"));
+            if (check.equals("yes")) {
+                System.out.print("Введите первое число: ");
+                calculator.setNumber1(console.nextLong());
+                System.out.print("Введите знак математической операции: ");
+                calculator.setOperator(console.next().charAt(0));
+                System.out.print("Введите второе число: ");
+                calculator.setNumber2(console.nextLong());
+                calculator.checkingOperator();
+            }
+            System.out.print("Хотите продолжить вычисления? [yes/no]: ");
+            check = console.next();
         }
     }
 }
