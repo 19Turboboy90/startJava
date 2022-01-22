af93f8aa8327e6037086e12de946b1a9287634f5
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-01-22 10:09:03 +0300

    changes have been made to the functionality of the CalculatorTest class and the getter in the Calculator class has been removed

--- lesson_2/Calculator/Calculator.java
+++ lesson_2/Calculator/Calculator.java
@@ -3,31 +3,19 @@ public class Calculator {
     private long number2;
     private char operator;
 
-    public long getNumber1() {
-        return number1;
-    }
-
     public void setNumber1(long number1) {
         this.number1 = number1;
-    }
-
-    public long getNumber2() {
-        return number2;
     }
 
     public void setNumber2(long number2) {
         this.number2 = number2;
     }
 
-    public char getOperator() {
-        return operator;
-    }
-
     public void setOperator(char operator) {
         this.operator = operator;
     }
 
-    public void asd() {
+    public void checkingOperator() {
         switch (operator) {
             case '+':
                 System.out.println(number1 + number2);
--- lesson_2/Calculator/CalculatorTest.java
+++ lesson_2/Calculator/CalculatorTest.java
@@ -4,25 +4,19 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Scanner console = new Scanner(System.in);
-        boolean isReplay = false;
-        while (!isReplay) {
+        String check = "";
+        while (!check.equals("no")) {
             System.out.print("Введите первое число: ");
             calculator.setNumber1(console.nextLong());
             System.out.print("Введите знак математической операции: ");
             calculator.setOperator(console.next().charAt(0));
             System.out.print("Введите второе число: ");
             calculator.setNumber2(console.nextLong());
-            calculator.asd();
-            System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-            String amv = console.next();
-            while (!amv.equals("no") && !amv.equals("yes")) {
+            calculator.checkingOperator();
+            do {
                 System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-                amv = console.next();
-                isReplay = false;
-            }
-            if (amv.equals("no")) {
-                isReplay = true;
-            }
+                check = console.next();
+            } while (!check.equals("yes") && !check.equals("no"));
         }
     }
 }
