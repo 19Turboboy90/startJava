edeb833504eb2666d7ce2fc7ac8012844fa4f867
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-01-24 16:09:04 +0300

    Added functionality changes in classes

--- lesson_2/GuessNumber/GuessNumber.java
+++ lesson_2/GuessNumber/GuessNumber.java
@@ -1,15 +1,38 @@
-public class GuessNumber {
-    int guessNumber = (int) (Math.random() * 100);
+import java.util.Scanner;
 
-    public void guess(Player player) {
-        if (guessNumber > player.getNumber()) {
-            System.out.println("Данное число меньше того, что загадал компьютер");
-        } else if (guessNumber < player.getNumber()) {
-            System.out.println("Данное число больше того, что загадал компьютер");
-        }
+public class GuessNumber {
+    private Player player1;
+    private Player player2;
+
+    public GuessNumber(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
     }
 
-    public void newGuess() {
-        guessNumber = (int) (Math.random() * 100);
+    public void startGame() {
+        Scanner console = new Scanner(System.in);
+        int guessNumber = (int) (Math.random() * 100 + 1);
+        while (true) {
+            System.out.print("Игрок " + player1.getName() + " вводит число: ");
+            player1.setNumber(console.nextInt());
+            if (guessNumber == player1.getNumber()) {
+                System.out.println("Игрок: " + player1.getName() + " победил!!! Угадав число " + guessNumber);
+                break;
+            } else if (guessNumber > player1.getNumber()) {
+                System.out.println("Данное число меньше того, что загадал компьютер");
+            } else {
+                System.out.println("Данное число больше того, что загадал компьютер");
+            }
+            System.out.print("Тгрок " + player2.getName() + " вводит число: ");
+            player2.setNumber(console.nextInt());
+            if (guessNumber == player2.getNumber()) {
+                System.out.println("Игрок: " + player1.getName() + " победил!!! Угадав число " + guessNumber);
+                break;
+            } else if (guessNumber > player2.getNumber()) {
+                System.out.println("Данное число меньше того, что загадал компьютер");
+            } else {
+                System.out.println("Данное число больше того, что загадал компьютер");
+            }
+        }
     }
 }
