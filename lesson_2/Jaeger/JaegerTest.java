3eac0004ef637e1b5335edb339a587d7406cc13b
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-01-25 12:15:20 +0300

    Added constructor, removed unnecessary setter and getter

--- lesson_2/Jaeger/JaegerTest.java
+++ lesson_2/Jaeger/JaegerTest.java
@@ -1,24 +1,7 @@
 public class JaegerTest {
     public static void main(String[] args) {
-        Jaeger romeoBlue = new Jaeger();
-        romeoBlue.setModelName("Romeo Blue");
-        romeoBlue.setMark("Mark-1");
-        romeoBlue.setOrigin("USA");
-        romeoBlue.setHeight(77.72f);
-        romeoBlue.setWeight(7775);
-        romeoBlue.setSpeed(2);
-        romeoBlue.setStrength(7);
-        romeoBlue.setArmor(6);
-
-        Jaeger louisBancroft = new Jaeger();
-        louisBancroft.setModelName("Louis Bancroft");
-        louisBancroft.setMark("Mark-1");
-        louisBancroft.setOrigin("Canada");
-        louisBancroft.setHeight(79);
-        louisBancroft.setWeight(2980);
-        louisBancroft.setSpeed(10);
-        louisBancroft.setStrength(10);
-        louisBancroft.setArmor(10);
+        Jaeger romeoBlue = new Jaeger("Romeo Blue","Mark-1","USA",77.72f,7775,2,7,6);
+        Jaeger louisBancroft = new Jaeger("Louis Bancroft","Mark-1","Canada",79,2980,10,10,10);
 
         System.out.println(louisBancroft.getModelName() + " Вышел на охоту");
         louisBancroft.scanningTerrain("Производится сканирование месности");
