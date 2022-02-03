486f2f62bcb1fb5355ebe4bebfbb819858527aaa
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-02-03 20:28:21 +0300

    the implementation of a mathematical expression has been moved to the calculator class

--- src/com/startJava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startJava/lesson_2_3_4/calculator/Calculator.java
@@ -1,7 +1,19 @@
 package com.startJava.lesson_2_3_4.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
-    public int calculate(int number1, int number2, char operator) {
+    public int calculate() {
+        int number1;
+        int number2;
+        char operator;
+        Scanner console = new Scanner(System.in);
+        System.out.print("Введите математическое выражение: ");
+        String mathExpression = console.nextLine();
+        String[] arrayMath = mathExpression.split(" ");
+        number1 = Integer.parseInt(arrayMath[0]);
+        operator = arrayMath[1].charAt(0);
+        number2 = Integer.parseInt(arrayMath[2]);
         switch (operator) {
             case '+':
                 return Math.addExact(number1, number2);
@@ -15,7 +27,8 @@ public class Calculator {
                 return (int) Math.IEEEremainder(number1, number2);
             case '^':
                 return (int) Math.pow(number1, number2);
-            default:  System.out.println("Введен неверный математический оператор");
+            default:
+                System.out.println("Введен неверный математический оператор");
         }
         return -1;
     }
--- src/com/startJava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startJava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -6,19 +6,13 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Scanner console = new Scanner(System.in);
-        String checkingResponse = "yes";
-        while (!checkingResponse.equals("no")) {
-            if (checkingResponse.equals("yes")) {
-                System.out.print("Введите математическое выражение: ");
-                String mathExpression = console.nextLine();
-                String[] arrayMath = mathExpression.split(" ");
-                int number1 = Integer.parseInt(arrayMath[0]);
-                char operator = arrayMath[1].charAt(0);
-                int number2 = Integer.parseInt(arrayMath[2]);
-                System.out.println(calculator.calculate(number1, number2, operator));
+        String option = "yes";
+        while (!option.equals("no")) {
+            if (option.equals("yes")) {
+                System.out.println(calculator.calculate());
             }
             System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-            checkingResponse = console.nextLine();
+            option = console.nextLine();
         }
     }
 }
